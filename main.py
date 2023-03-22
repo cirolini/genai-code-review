3ee661968c58be6daa1d5cf7a0bea0522f0c9a28
Masaya Kataoka <ms.kataoka@gmail.com>
2023-03-22 23:51:40 +0900

    add review mode
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- main.py
+++ main.py
@@ -89,6 +89,43 @@ def patch():
             pull_request.create_issue_comment(f"ChatGPT was unable to process the response about {file_name}")
 
 
+def review():
+    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
+    pull_request = repo.get_pull(int(args.github_pr_id))
+
+    content = get_content_patch()
+
+    if len(content) == 0:
+        pull_request.create_issue_comment(f"Patch file does not contain any changes")
+        return
+
+    parsed_text = content.split("diff")
+
+    for diff_text in parsed_text:
+        if len(diff_text) == 0:
+            continue
+
+        try:
+            file_name = diff_text.split("b/")[1].splitlines()[0]
+            print(file_name)
+
+            response = openai.Completion.create(
+                engine=args.openai_engine,
+                prompt=(f"Please review code diff:\n```{diff_text}```"),
+                temperature=float(args.openai_temperature),
+                max_tokens=int(args.openai_max_tokens)
+            )
+            print(response)
+            print(response['choices'][0]['text'])
+
+            pull_request.create_issue_comment(
+                f"ChatGPT's response about ``{file_name}``:\n {response['choices'][0]['text']}")
+        except Exception as e:
+            error_message = str(e)
+            print(error_message)
+            pull_request.create_issue_comment(f"ChatGPT was unable to process the response about {file_name}")
+
+
 def get_content_patch():
     url = f"https://api.github.com/repos/{os.getenv('GITHUB_REPOSITORY')}/pulls/{args.github_pr_id}"
     print(url)
@@ -111,3 +148,6 @@ if (args.mode == "files"):
 
 if (args.mode == "patch"):
     patch()
+
+if (arg.mode == "review"):
+    review()
